e5a88efcf3fedb40bd11eb013d3ea72b93bc88dc
Jae Sang Ha <JaeSang.Ha@morningstar.com>
2022-02-25 19:51:32 -0800

    key in .env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7TD6ieM-Tuk2SeSLQN0MF_qBEt11kb-k",
-    authDomain: "twitterclone-cfca3.firebaseapp.com",
-    projectId: "twitterclone-cfca3",
-    storageBucket: "twitterclone-cfca3.appspot.com",
-    messagingSenderId: "993512346190",
-    appId: "1:993512346190:web:001eda3a331f772c85e91f",
-    measurementId: "G-RX08YB4DG6"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
   export default firebase.initializeApp(firebaseConfig);
